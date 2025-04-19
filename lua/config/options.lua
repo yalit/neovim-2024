d6a37a7039591fa510263917ce6fe3eb458a5a81
yannick <y.alsberge@yalit.be>
2025-04-19 12:22:36 +0200

    feat : set noexpandtab

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.cmd("set noexpandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
